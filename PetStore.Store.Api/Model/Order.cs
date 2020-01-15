d52ba6c7054545de7d1efa4f550326b2c5e3683d
Henrik <henrik.mosgaard.jensen@gmail.com>
2020-01-15 23:32:42 +0100

    add model objects, queries and commands

---
+++ PetStore.Store.Api/Model/Order.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PetStore.Store.Api.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int PetId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ShipDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public bool Complete { get; set; }
+    }
+}
---
+++ PetStore.Store.Api/Model/Queries/GetOrder.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using PetStore.Store.Api.Infrastructure;
+
+namespace PetStore.Store.Api.Model.Queries
+{
+    public interface IGetOrder
+    {
+        Order Execute(int orderId);
+    }
+    
+    public class GetOrder : IGetOrder
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        
+        public GetOrder(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public Order Execute(int orderId)
+        {
+            var sql = @" /* PetStore.Store.Api */
+select o.Id, o.Status, o.PetId, o.Quantity, o.ShipDate, o.Complete 
+from orders.order o
+where o.IsDelete = false
+and o.id = @id";
+
+            using (var connection = _connectionFactory.Get())
+            {
+               return connection.Query<Order>(sql, new {id = orderId}).FirstOrDefault();
+            }
+        }
+    }
+}
