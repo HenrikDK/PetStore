d1337e5e428778982d5a02ff160d516f4a58dba8
HenrikDK <commander.john.crichton@gmail.com>
2020-01-15 23:30:58 +0100

    add model objects, queries and commands

---
+++ PetStore.Pet.Api/Model/Pet.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace PetStore.Pet.Api.Model
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public PetCategory Category { get; set; }
+        public string Name { get; set; }
+        public IList<string> PhotoUrls { get; set; }
+        public string Tags { get; set; }
+        public PetStatus Status { get; set; }
+    }
+}
---
+++ PetStore.Pet.Api/Model/Queries/GetPetByStatus.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using PetStore.Pet.Api.Infrastructure;
+
+namespace PetStore.Pet.Api.Model.Queries
+{
+    public interface IGetPetByStatus
+    {
+        IList<Pet> Execute(PetStatus status);
+    }
+    
+    public class GetPetByStatus : IGetPetByStatus
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        
+        public GetPetByStatus(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public IList<Pet> Execute(PetStatus status)
+        {
+            var sql = @" /* PetStore.Pet.Api */
+select p.id, p.Name, p.Category, p.Status, p.Tags 
+from pets.pet p
+where p.IsDelete = false
+and p.status = @status";
+
+            using (var connection = _connectionFactory.Get())
+            {
+                return connection.Query<Pet>(sql, new {status}).ToList();
+            }
+        }
+    }
+}
