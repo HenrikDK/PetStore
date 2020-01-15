51210473550d0d4c4f8982b27b6ad590f43b7fbf
HenrikDK <commander.john.crichton@gmail.com>
2020-01-15 23:34:48 +0100

    test Store.Api commands and queries

---
+++ PetStore.Test/Store.Api/CommandsTests.cs
@@ -0,0 +1,48 @@
+using System;
+using FluentAssertions;
+using Lamar;
+using NUnit.Framework;
+using PetStore.Store.Api.Infrastructure;
+using PetStore.Store.Api.Model;
+using PetStore.Store.Api.Model.Commands;
+
+namespace PetStore.Test.Store.Api
+{
+    public class CommandsTests
+    {
+        private Container _container;
+
+        [SetUp]
+        public void Setup()
+        {
+            _container = new Container(new ApiRegistry());
+        }
+
+        [Test]
+        public void Should_delete_order()
+        {
+            var deleteOrder = _container.GetInstance<IDeleteOrder>();
+
+            deleteOrder.Execute(1);
+        }
+        
+        [Test]
+        public void Should_insert_order()
+        {
+            var insertOrder = _container.GetInstance<IInsertOrder>();
+
+            var order = new Order
+            {
+                Complete = true,
+                Quantity = 1,
+                Status = OrderStatus.Shipped,
+                ShipDate = DateTime.Now.AddDays(-19),
+                PetId = 123
+            };
+            
+            var petId = insertOrder.Execute(order);
+
+            petId.Should().NotBe(0);
+        }
+    }
+}
