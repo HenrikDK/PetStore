1c2bbf6b6731c164dd635462c3bedcafe79623a1
HenrikDK <commander.john.crichton@gmail.com>
2020-01-15 23:32:33 +0100

    add infrastructure

---
+++ PetStore.Store.Api/Infrastructure/ApiRegistry.cs
@@ -0,0 +1,23 @@
+using Lamar;
+
+namespace PetStore.Store.Api.Infrastructure
+{
+    public class ApiRegistry : ServiceRegistry
+    {
+        public ApiRegistry()
+        {
+            Scan(x =>
+            {
+                x.AssemblyContainingType<Program>();
+                
+                x.WithDefaultConventions();
+
+                x.LookForRegistries();
+                
+                x.ExcludeType<ApiRegistry>();
+            });
+
+            For<IConnectionFactory>().Use<ConnectionFactory>().Singleton();
+        }
+    }
+}
