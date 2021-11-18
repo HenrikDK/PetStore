6e1b38b7dc1813d67b80bb1b9ece460c159218cf
HenrikDK <HenrikDK@some.place>
2021-11-18 22:00:29 +0100

    cleanup test

--- PetStore.Test/User.Api/CommandsTests.cs
+++ PetStore.Test/User.Api/CommandsTests.cs
@@ -1,66 +1,62 @@
-using FluentAssertions;
-using Lamar;
-using NUnit.Framework;
 using PetStore.User.Api.Infrastructure;
 using PetStore.User.Api.Model;
 using PetStore.User.Api.Model.Commands;
 
-namespace PetStore.Test.User.Api
+namespace PetStore.Test.User.Api;
+
+public class CommandsTests
 {
-    public class CommandsTests
+    private Container _container;
+
+    [SetUp]
+    public void Setup()
     {
-        private Container _container;
+        _container = new Container(new ApiRegistry());
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            _container = new Container(new ApiRegistry());
-        }
+    [Test]
+    public void Should_delete_user()
+    {
+        var deleteUser = _container.GetInstance<IDeleteUser>();
 
-        [Test]
-        public void Should_delete_user()
-        {
-            var deleteUser = _container.GetInstance<IDeleteUser>();
-
-            deleteUser.Execute("PETE");
-        }
+        deleteUser.Execute("PETE");
+    }
         
-        [Test]
-        public void Should_insert_user()
+    [Test]
+    public void Should_insert_user()
+    {
+        var insertUser = _container.GetInstance<IInsertUser>();
+
+        var user = new PetStore.User.Api.Model.User
         {
-            var insertUser = _container.GetInstance<IInsertUser>();
-
-            var user = new PetStore.User.Api.Model.User
-            {
-                Email = "spam@real.com",
-                Phone = "12345678",
-                FirstName = "Mayo",
-                LastName = "Pete",
-                Status = UserStatus.Active,
-                UserName = "PETE",
-            };
+            Email = "spam@real.com",
+            Phone = "12345678",
+            FirstName = "Mayo",
+            LastName = "Pete",
+            Status = UserStatus.Active,
+            UserName = "PETE",
+        };
             
-            var petId = insertUser.Execute(user);
+        var petId = insertUser.Execute(user);
 
-            petId.Should().NotBe(0);
-        }
+        petId.Should().NotBe(0);
+    }
         
-        [Test]
-        public void Should_update_user()
-        {
-            var updateUser = _container.GetInstance<IUpdateUser>();
+    [Test]
+    public void Should_update_user()
+    {
+        var updateUser = _container.GetInstance<IUpdateUser>();
 
-            var user = new PetStore.User.Api.Model.User
-            {
-                Email = "spam@real.com",
-                Phone = "12345678",
-                FirstName = "Mayo",
-                LastName = "Pete",
-                Status = UserStatus.Inactive,
-                UserName = "PETE",
-            };
+        var user = new PetStore.User.Api.Model.User
+        {
+            Email = "spam@real.com",
+            Phone = "12345678",
+            FirstName = "Mayo",
+            LastName = "Pete",
+            Status = UserStatus.Inactive,
+            UserName = "PETE",
+        };
             
-            updateUser.Execute(user);
-        }
+        updateUser.Execute(user);
     }
 }
