d26d3d3e307054d189735709ddfbf60c155a88d8
HenrikDK <HenrikDK@some.place>
2021-11-18 22:00:41 +0100

    cleanup user api

--- PetStore.User.Api/Model/Commands/InsertUser.cs
+++ PetStore.User.Api/Model/Commands/InsertUser.cs
@@ -1,34 +1,32 @@
-using Dapper;
 using PetStore.User.Api.Infrastructure;
 
-namespace PetStore.User.Api.Model.Commands
-{
-    public interface IInsertUser
-    {
-        int Execute(User user);
-    }
-    
-    public class InsertUser : IInsertUser
-    {
-        private readonly IConnectionFactory _connectionFactory;
-        
-        public InsertUser(IConnectionFactory connectionFactory)
-        {
-            _connectionFactory = connectionFactory;
-        }
+namespace PetStore.User.Api.Model.Commands;
 
-        public int Execute(User user)
-        {
-            var sql = @" /* PetStore.User.Api */
+public interface IInsertUser
+{
+    int Execute(User user);
+}
+    
+public class InsertUser : IInsertUser
+{
+    private readonly IConnectionFactory _connectionFactory;
+        
+    public InsertUser(IConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public int Execute(User user)
+    {
+        var sql = @" /* PetStore.User.Api */
 insert into users.user (username, firstname, lastname, email, passwordhash, salt, phone, status, created, createdby) 
 values (@username, @firstname, @lastname, @email, @passwordhash, @salt, @phone, @status, current_timestamp, 'PetStore.User.Api');
 
 select currval('users.user_id_seq')";
 
-            using (var connection = _connectionFactory.Get())
-            {
-                return connection.ExecuteScalar<int>(sql, user);
-            }
+        using (var connection = _connectionFactory.Get())
+        {
+            return connection.ExecuteScalar<int>(sql, user);
         }
     }
 }
