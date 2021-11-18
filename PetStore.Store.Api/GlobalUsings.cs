f7c859b3c0aba0e07e740d219c23ba442fd154f4
HenrikDK <commander.john.crichton@gmail.com>
2021-11-18 21:59:52 +0100

    add global usings

---
+++ PetStore.Store.Api/GlobalUsings.cs
@@ -0,0 +1,21 @@
+global using System;
+global using System.Data;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Reflection;
+
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.OpenApi.Models;
+
+global using Dapper;
+global using Lamar;
+global using Lamar.Microsoft.DependencyInjection;
+global using Prometheus;
