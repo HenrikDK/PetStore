a62088fbf468d7d25ccd2bd4f757efdb2cf02e9e
HenrikDK <commander.john.crichton@gmail.com>
2021-11-18 22:00:29 +0100

    cleanup test

--- PetStore.Test/Pet.Api/QueriesTests.cs
+++ PetStore.Test/Pet.Api/QueriesTests.cs
@@ -1,41 +1,37 @@
-using FluentAssertions;
-using Lamar;
-using NUnit.Framework;
 using PetStore.Pet.Api.Infrastructure;
 using PetStore.Pet.Api.Model;
 using PetStore.Pet.Api.Model.Queries;
 
-namespace PetStore.Test.Pet.Api
+namespace PetStore.Test.Pet.Api;
+
+public class QueriesTests
 {
-    public class QueriesTests
+    private Container _container;
+
+    [SetUp]
+    public void Setup()
     {
-        private Container _container;
+        _container = new Container(new ApiRegistry());
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            _container = new Container(new ApiRegistry());
-        }
+    [Test]
+    public void Should_get_pet()
+    {
+        var getPet = _container.GetInstance<IGetPet>();
 
-        [Test]
-        public void Should_get_pet()
-        {
-            var getPet = _container.GetInstance<IGetPet>();
+        var pet = getPet.Execute(1);
 
-            var pet = getPet.Execute(1);
-
-            pet.Should().NotBeNull();
-        }
+        pet.Should().NotBeNull();
+    }
         
-        [Test]
-        public void Should_get_pet_by_status()
-        {
-            var getPet = _container.GetInstance<IGetPetByStatus>();
+    [Test]
+    public void Should_get_pet_by_status()
+    {
+        var getPet = _container.GetInstance<IGetPetByStatus>();
 
-            var pet = getPet.Execute(PetStatus.Available);
+        var pet = getPet.Execute(PetStatus.Available);
 
-            pet.Should().NotBeNull();
-            pet.Count.Should().BeGreaterThan(0);
-        }
+        pet.Should().NotBeNull();
+        pet.Count.Should().BeGreaterThan(0);
     }
 }
